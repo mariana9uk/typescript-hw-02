fd73fabd3de2c7a3309832abc5ad744e91d3c94c
mariana9uk <maryanagolyuk@gmail.com>
2023-12-25 19:25:27 +0200

    finished

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Info{
+  title:string
+}
 
-class Component {
+class Component <T extends Info> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <Info>{
   pageInfo () {
     console.log(this.props.title);
   }
